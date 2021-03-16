1dbc20c75e88a14846e2bdaf518ac30174c01178
Nbiba Bedis <bedisnbiba@gmail.com>
2021-03-16 12:12:42 +0100

    Clean client code

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,8 +3,10 @@ use rustyline::Editor;
 use std::io::prelude::*;
 use std::net::TcpStream;
 
-fn main() -> std::io::Result<()> {
-    let server = std::env::args().nth(1).unwrap();
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+fn main() -> Result<()> {
+    let server = std::env::args().nth(1).expect("server address is required");
     let mut rl = Editor::<()>::new();
     if rl.load_history("history.txt").is_err() {
         println!("No previous history.");
@@ -12,7 +14,6 @@ fn main() -> std::io::Result<()> {
 
     let mut output = [0; 512];
     let mut data: Vec<u8> = vec![];
-    let mut vim_flag = false;
     loop {
         let mut stream_write = TcpStream::connect(&server)?;
         let mut stream_read = TcpStream::connect(&server)?;
@@ -27,16 +28,16 @@ fn main() -> std::io::Result<()> {
             }
         };
         rl.add_history_entry(line.as_str());
-        rl.save_history("history.txt").unwrap();
+        rl.save_history("history.txt")?;
 
         stream_write.write_all(line.as_bytes())?;
         drop(stream_write);
+
         loop {
             let read_n = stream_read.read(&mut output)?;
             if output.starts_with(b"?vim") {
                 data.clear();
                 data.extend(&output[..read_n]);
-                vim_flag = true;
                 loop {
                     let read_n = stream_read.read(&mut output)?;
                     data.extend(&output[..read_n]);
@@ -44,32 +45,38 @@ fn main() -> std::io::Result<()> {
                         break;
                     }
                 }
-            }
-            if read_n == 0 {
-                break;
-            }
-            if vim_flag {
-                let data = String::from_utf8(data.clone()).unwrap();
-                let mut data = data.splitn(2, "???");
-                let file_name = data.next().unwrap().strip_prefix("?vim").unwrap().trim();
-                let data = data.next().unwrap();
+                let orig_data = String::from_utf8_lossy(&data);
+                let mut data = orig_data.splitn(2, "???");
+                let file_name = data
+                    .next()
+                    .ok_or(format!("filename is missing, msg: {}", orig_data))?
+                    .strip_prefix("?vim")
+                    .ok_or(format!("?vim is missing, msg: {}", orig_data))?
+                    .trim();
+                let data = data
+                    .next()
+                    .ok_or(format!("data is missing, msg: {}", orig_data))?;
 
                 let path = std::env::temp_dir().join(file_name);
                 std::fs::write(&path, &data)?;
+
                 std::process::Command::new("nvim")
                     .arg(&path)
                     .spawn()?
                     .wait()?;
+
                 let mut stream_write = TcpStream::connect(&server)?;
                 let _stream_read = TcpStream::connect(&server)?;
 
                 let client_data = std::fs::read_to_string(&path)?;
                 stream_write.write_all(client_data.as_bytes())?;
 
-                vim_flag = false;
-            } else {
-                println!("{}", &String::from_utf8_lossy(&output[..read_n]));
+                break;
             }
+            if read_n == 0 {
+                break;
+            }
+            println!("{}", &String::from_utf8_lossy(&output[..read_n]));
         }
     }
 }
